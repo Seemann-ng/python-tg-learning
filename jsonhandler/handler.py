9f8a70a6a0e8d756b605d9c893494c18941f58e6
Ilia <ilyarus199939@gmail.com>
2024-09-21 21:12:35 +0600

    Add logging to handler.py

--- jsonhandler/handler.py
+++ jsonhandler/handler.py
@@ -1,5 +1,13 @@
 import json
+import logging
 from typing import List, Any, Dict
+
+logging.basicConfig(
+    filename="log.txt",
+    filemode="a",
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 
 class JSONHandler_:
@@ -14,8 +22,10 @@ class JSONHandler_:
             Data from .json file.
 
         """
+        logging.info("json_reader() enter.")
         with open(filename, "r", encoding="utf-8") as openfile:
             readable_data = json.load(openfile)
+        logging.info("json_reader() exit.")
         return readable_data
 
     @staticmethod
@@ -27,8 +37,10 @@ class JSONHandler_:
             written: Data to be written into .json file.
 
         """
+        logging.info("json_writer() enter.")
         with open(filename, "w", encoding="utf-8") as openjson:
             json.dump(written, openjson, indent=4, ensure_ascii=False)
+        logging.info("json_writer() exit.")
 
     @staticmethod
     def json_list_appender(file_changed: str, added: Dict[str, str | int] | List[Dict[str, str | int]]) -> None:
@@ -39,9 +51,11 @@ class JSONHandler_:
             added: List or dictionary with data that is to be added to .json file.
 
         """
+        logging.info("json_list_appender() enter.")
         json_being_changed: List[Any] = JSONHandler_.json_reader(file_changed)
         json_being_changed.append(added)
         JSONHandler_.json_writer(file_changed, json_being_changed)
+        logging.info("json_list_appender() exit.")
 
     @staticmethod
     def json_dict_updater(file_changed: str, added: Dict[str, Any]) -> None:
@@ -52,9 +66,11 @@ class JSONHandler_:
             added: Dictionary with data that is to be added to .json file.
 
         """
+        logging.info("json_dict_updater() enter.")
         json_being_changed: Dict[str, Any] = JSONHandler_.json_reader(file_changed)
         json_being_changed.update(added)
         JSONHandler_.json_writer(file_changed, json_being_changed)
+        logging.info("json_dict_updater() exit.")
 
     @staticmethod
     def json_dict_popper(file_changed: str, popped: str) -> None:
@@ -65,6 +81,8 @@ class JSONHandler_:
             popped: Key of item to be popped.
 
         """
+        logging.info("json_dict_popper() enter.")
         json_being_changed: Dict[str, Any] = JSONHandler_.json_reader(file_changed)
         json_being_changed.pop(popped)
         JSONHandler_.json_writer(file_changed, json_being_changed)
+        logging.info("json_dict_popper() exit.")
